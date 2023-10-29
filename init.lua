d08d68dd4cc70c6e3354187b027329c744994fff
Andrey Voznyuk <andrey.voznyuk.dev@gmail.com>
2023-10-29 14:04:46 -0400

    Basic config.

---
+++ init.lua
@@ -0,0 +1,39 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require('options')
+require('plugins')
+
+vim.g.material_style = "darker"
+vim.cmd 'colorscheme material'
+
+-- BEGIN: Plugins setup
+require('lualine').setup {}
+
+require("nvim-tree").setup {}
+
+require("project_nvim").setup {}
+
+require('telescope').load_extension('media_files')
+require('telescope').load_extension('projects')
+
+require('mason').setup {}
+require('mason-lspconfig').setup {}
+
+require('nvim-autopairs').setup {}
+
+require('gitsigns').setup()
+
+require('cmp_setup')
+require('ts_setup')
+
+local wk = require("which-key")
+
+-- END: Plugins setup
+
+require('keymaps')
+wk.register(mappings, {})
---
+++ lua/plugins.lua
@@ -0,0 +1,81 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+plugins = {
+    -- Themes
+    'marko-cerovac/material.nvim',
+
+    -- Status line
+    'nvim-lualine/lualine.nvim',
+
+    -- File Tree
+    'nvim-tree/nvim-tree.lua',
+
+    -- Icons
+    'nvim-tree/nvim-web-devicons',
+
+    -- Terminal
+    {'akinsho/toggleterm.nvim', version = "*", config = true},
+    
+    -- Project
+    'ahmedkhalf/project.nvim',
+    
+    -- Telescope
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    'nvim-telescope/telescope-media-files.nvim',
+    
+    -- Minimap
+    'wfxr/minimap.vim',
+
+    -- Tabs
+    'nanozuki/tabby.nvim',
+
+    -- LSP
+    'williamboman/mason.nvim',
+    "williamboman/mason-lspconfig.nvim",
+    'neovim/nvim-lspconfig',
+
+    -- CMP
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+
+    -- Treesitter
+    'nvim-treesitter/nvim-treesitter',
+
+    -- Autopairs
+    'windwp/nvim-autopairs',
+
+    -- git
+    'lewis6991/gitsigns.nvim',
+
+    -- which key
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}
+
+require("lazy").setup(plugins)
