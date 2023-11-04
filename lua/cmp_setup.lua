9b6ea974dd9f70af03350f77d5596ab261941c1b
Andrey Voznyuk <andrey.voznyuk.dev@gmail.com>
2023-11-04 13:20:48 -0400

    Using TAB for selecting the next cmp item.

--- lua/cmp_setup.lua
+++ lua/cmp_setup.lua
@@ -18,6 +18,7 @@
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<Tab>'] = cmp.mapping.select_next_item(),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
