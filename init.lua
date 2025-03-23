07b34169fbe7f87f83e65521b8ca148c47808e67
Andrey Voznyuk <andrey.voznyuk.dev@gmail.com>
2025-03-23 10:09:52 -0600

    Initial config.

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- Set leader key to Space
+vim.g.mapleader = " "
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Enable syntax highlighting
+vim.cmd("syntax enable")
+
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Enable mouse support
+vim.opt.mouse = "a"
