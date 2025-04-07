d7961c28ad1d491503271151d55881aa40f7a668
Andrey Voznyuk <andrey.voznyuk.dev@gmail.com>
2025-04-06 23:01:29 -0600

    Kanagawa theme, mason configuration.

--- init.lua
+++ init.lua
@@ -23,3 +23,7 @@ vim.keymap.set("n", "<leader>r", ":luafile " .. config_path .. "<CR>", { silent 
 
 -- Lazy plugins manager setup
 require('config.lazy')
+
+require('config.setup')
+
+vim.cmd("colorscheme kanagawa")
